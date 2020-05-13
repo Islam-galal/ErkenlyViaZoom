622bee54483a9122b155ea5fc42b20455fc3107d
Islam-galal <58890338+Islam-galal@users.noreply.github.com>
2020-05-13 22:57:57 +0200

    Initial commit

---
+++ app/src/main/java/com/example/erkenlyviazoom/BaseActivity.java
@@ -0,0 +1,16 @@
+package com.example.erkenlyviazoom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_base );
+
+
+    }
+}
